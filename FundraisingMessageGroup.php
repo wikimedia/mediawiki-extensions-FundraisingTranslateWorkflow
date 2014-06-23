36b83eb924974cbbafee3946a6f10414035fcfac
Adam Roses Wight <awight@wikimedia.org>
2014-06-23 15:46:11 -0700

    Hideous workaround to customize translate workflow permissions
    Change-Id: I81ed68c38adc5fdfa633fb66aead7561bdd7fafa

---
+++ FundraisingMessageGroup.php
@@ -0,0 +1,78 @@
+<?php
+namespace FundraisingTranslateWorkflow;
+
+use \IContextSource;
+use \MessageGroup;
+use \MessageGroupStates;
+
+/**
+ * Wrapper to modify message group behavior
+ */
+class FundraisingMessageGroup implements MessageGroup {
+	/**
+	 * TranslatePostInitGroups hook handler
+	 *
+	 * Wrap translation groups we want to mutate.
+	 *
+	 * @param array $list
+	 * @return bool
+	 */
+	public static function onTranslatePostInitGroups( &$groups, &$deps, &$autoload ) {
+		global $wgFundraisingTranslateWorkflowPagePatterns;
+
+		foreach ( $groups as $name => &$group ) {
+			foreach ( $wgFundraisingTranslateWorkflowPagePatterns as $pattern ) {
+				if ( preg_match( $pattern, $name ) ) {
+					$group = new FundraisingMessageGroup( $group );
+					break;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	public function __construct( $group ) {
+		$this->group = $group;
+	}
+
+	/**
+	 * Modify workflow permissions
+	 */
+	public function getMessageGroupStates() {
+		global $wgFundraisingTranslateWorkflowPublishRight;
+
+		$states = $this->group->getMessageGroupStates()->getStates();
+
+		$states['published']['right'] = $wgFundraisingTranslateWorkflowPublishRight;
+
+		return new MessageGroupStates( $states );
+	}
+
+	/**
+	 * Delegate everything else to the pseudo-parent object
+	 */
+	public function __call( $method, $args ) {
+		return call_user_func_array( array( $this->group, $method ), $args );
+	}
+
+    function getConfiguration() { return $this->__call( __FUNCTION__, func_get_args() ); }
+    function getId() { return $this->__call( __FUNCTION__, func_get_args() ); }
+    function getLabel( IContextSource $context = null ) { return $this->__call( __FUNCTION__, func_get_args() ); }
+    function getDescription( IContextSource $context = null ) { return $this->__call( __FUNCTION__, func_get_args() ); }
+    function getIcon() { return $this->__call( __FUNCTION__, func_get_args() ); }
+    function getNamespace() { return $this->__call( __FUNCTION__, func_get_args() ); }
+    function isMeta() { return $this->__call( __FUNCTION__, func_get_args() ); }
+    function exists() { return $this->__call( __FUNCTION__, func_get_args() ); }
+    function getFFS() { return $this->__call( __FUNCTION__, func_get_args() ); }
+    function getChecker() { return $this->__call( __FUNCTION__, func_get_args() ); }
+    function getMangler() { return $this->__call( __FUNCTION__, func_get_args() ); }
+    function initCollection( $code ) { return $this->__call( __FUNCTION__, func_get_args() ); }
+    function load( $code ) { return $this->__call( __FUNCTION__, func_get_args() ); }
+    function getDefinitions() { return $this->__call( __FUNCTION__, func_get_args() ); }
+    function getTags( $type = null ) { return $this->__call( __FUNCTION__, func_get_args() ); }
+    function getMessage( $key, $code ) { return $this->__call( __FUNCTION__, func_get_args() ); }
+    function getSourceLanguage() { return $this->__call( __FUNCTION__, func_get_args() ); }
+    function getTranslatableLanguages() { return $this->__call( __FUNCTION__, func_get_args() ); }
+    function getTranslationAids() { return $this->__call( __FUNCTION__, func_get_args() ); }
+}
---
+++ FundraisingTranslateWorkflow.php
@@ -0,0 +1,29 @@
+<?php
+
+$wgExtensionCredits['other'][] = array(
+	'path' => __FILE__,
+	'name' => 'FundraisingTranslateWorkflow',
+	'author' => array(
+		'Adam Roses Wight',
+	),
+	'version' => '0.1',
+	'url' => 'https://www.mediawiki.org/wiki/Extension:FundraisingTranslateWorkflow',
+	'descriptionmsg' => 'fundraising-translate-workflow-desc',
+	'license-name' => 'GPLv2',
+);
+
+$wgAutoloadClasses['FundraisingTranslateWorkflow\FundraisingMessageGroup'] = __DIR__ . '/FundraisingMessageGroup.php';
+
+$wgHooks['TranslatePostInitGroups'][] = 'FundraisingTranslateWorkflow\FundraisingMessageGroup::onTranslatePostInitGroups';
+
+/**
+ * Message group titles matching these regexes will be wrapped by our hideous thing.
+ */
+$wgFundraisingTranslateWorkflowPagePatterns = array(
+	'|^page-Fundraising/|',
+);
+
+/**
+ * Permission needed to set our message groups to "published".
+ */
+$wgFundraisingTranslateWorkflowPublishRight = 'translate-manage';
